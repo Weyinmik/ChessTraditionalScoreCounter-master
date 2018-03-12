565cfe8cc329551312e2a364b2441ffd714fbc69
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-03-12 22:00:49 +0100

    Initial commit 4 - codes reformatted and comments updated

--- app/src/main/java/com/example/android/chesstraditionalscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/chesstraditionalscorecounter/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.android.chesstraditionalscorecounter;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,15 +9,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * Utilized variables for team White and Black scores are declared and initialised to zero (0).
+     */
     double scoreWhite = 0;
     double scoreBlack = 0;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
     }
 
     /**
@@ -26,13 +28,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState ( savedInstanceState );
+        savedInstanceState.putDouble ( "score for white", scoreWhite );
+        savedInstanceState.putDouble ( "score for black", scoreBlack );
 
-
-            savedInstanceState.putDouble ( "score for white", scoreWhite );
-            savedInstanceState.putDouble ( "score for black", scoreBlack );
-
-        }
-
+    }
 
     /**
      * Helpful for screen rotation, ensures that data is saved when screen rotates horizontally and vertically
@@ -48,87 +47,87 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * WHITE
+     * TEAM WHITE
      */
 
     /**
      * This increases the point by 1 when the +1WIN Button in White is clicked
      */
-    public void submitWinWhite(View view){
+    public void submitWinWhite(View view) {
         scoreWhite = scoreWhite + 1;
-        displayForWhite (scoreWhite);
+        displayForWhite ( scoreWhite );
 
     }
 
     /**
      * This increases the points by 0.5 when the 1/2Draw Button in White is clicked
      */
-    public void submitDrawWhite (View view){
+    public void submitDrawWhite(View view) {
         scoreWhite = scoreWhite + 0.5;
-        displayForWhite(scoreWhite);
+        displayForWhite ( scoreWhite );
     }
 
     /**
      * This increases the point by 0 when the +0LOSS Button in White is clicked
      */
-    public void submitLossWhite (View view){
+    public void submitLossWhite(View view) {
         scoreWhite = scoreWhite + 0;
-        displayForWhite(scoreWhite);
+        displayForWhite ( scoreWhite );
     }
 
     /**
-     * BLACK
+     * TEAM BLACK
      */
 
     /**
      * This increases the point by 1 when the +1WIN Button in Black is clicked
      */
-    public void submitWinBlack (View view){
+    public void submitWinBlack(View view) {
         scoreBlack = scoreBlack + 1;
-        displayForBlack(scoreBlack);
+        displayForBlack ( scoreBlack );
     }
 
     /**
      * This increases the point by 0.5 when the +1WIN Button in Black is clicked
      */
-    public void submitDrawBlack (View view){
+    public void submitDrawBlack(View view) {
         scoreBlack = scoreBlack + 0.5;
-        displayForBlack(scoreBlack);
+        displayForBlack ( scoreBlack );
     }
 
     /**
      * This increases the point by 0 when the +0LOSS Button in Black is clicked
      */
-    public void submitLossBlack (View view){
+    public void submitLossBlack(View view) {
         scoreBlack = scoreBlack + 0;
-        displayForBlack(scoreBlack);
+        displayForBlack ( scoreBlack );
     }
 
     /**
-     * RESET SCORE
+     * RESET SCORE FOR BOTH TEAM A AND TEAM B
      */
-    public void resetScore (View view){
+    public void resetScore(View view) {
         scoreWhite = 0;
         scoreBlack = 0;
-        displayForWhite(scoreWhite);
-        displayForBlack(scoreBlack);
+        displayForWhite ( scoreWhite );
+        displayForBlack ( scoreBlack );
     }
 
 
     /**
-     * Displays the given score for White.
+     * Displays the given score for Team White.
      */
     public void displayForWhite(double score) {
-        TextView scoreView = (TextView) findViewById(R.id.white_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreView = (TextView) findViewById ( R.id.white_score );
+        scoreView.setText ( String.valueOf ( score ) );
     }
 
     /**
-     * Displays the given score for Black.
+     * Displays the given score for Team Black.
      */
     public void displayForBlack(double score) {
-        TextView scoreView = (TextView) findViewById(R.id.black_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreView = (TextView) findViewById ( R.id.black_score );
+        scoreView.setText ( String.valueOf ( score ) );
     }
 }
 
